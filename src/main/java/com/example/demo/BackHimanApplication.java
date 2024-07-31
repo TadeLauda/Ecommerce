fb56783ed4e788865095a4772b9d11d59bb6c5e2
TadeLauda <tadeolaudadio2003@gmail.com>
2024-07-31 16:47:05 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/BackHimanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackHimanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackHimanApplication.class, args);
+	}
+
+}
